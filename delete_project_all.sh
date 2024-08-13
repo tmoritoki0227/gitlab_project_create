b6249d3e7decb2f19459450d2e8866dc7637e80d
tmoritoki0227 <tmoritoki0227@gmail.com>
2024-08-13 14:41:09 +0900

    Update delete_project_all.sh

--- delete_project_all.sh
+++ delete_project_all.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -ex
 
-for project_id in $(curl --header "PRIVATE-TOKEN: glpat-wTscsa5Y6VrMVHDA7DsP" "http://35.75.155.254/gitlab/api/v4/projects?simple=true&per_page=100" | jq -r '.[].id'); do
+# SSMの設定が必要
+GITLAB_URL="http://${gitlab_url}/gitlab/api/v4"
+TOKEN=${gitlab_access_token}
+
+# per_page=100の意味がわかってない。リポジトリが１００までしか取得できないとか？
+for project_id in $(curl --header "PRIVATE-TOKEN: $TOKEN" "${GITLAB_URL}/projects?simple=true&per_page=100" | jq -r '.[].id'); do
   if [ "$project_id" == "125" ]; then # project_create
     echo "Skipping project with ID: $project_id"
     continue
@@ -13,6 +18,6 @@ for project_id in $(curl --header "PRIVATE-TOKEN: glpat-wTscsa5Y6VrMVHDA7DsP" "h
   fi
 
   echo "Deleting project with ID: $project_id"
-  curl --request DELETE --header "PRIVATE-TOKEN: glpat-wTscsa5Y6VrMVHDA7DsP" "http://35.75.155.254/gitlab/api/v4/projects/${project_id}"
+  curl --request DELETE --header "PRIVATE-TOKEN: $TOKEN" "${GITLAB_URL}/projects/${project_id}"
 done
 sleep 1
