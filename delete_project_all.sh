4ed25490f0f17d91973af44374fe82a37c33290e
tmoritoki0227 <tmoritoki0227@gmail.com>
2024-08-13 14:43:54 +0900

    Update delete_project_all.sh

--- delete_project_all.sh
+++ delete_project_all.sh
@@ -6,7 +6,7 @@ GITLAB_URL="http://${gitlab_url}/gitlab/api/v4"
 TOKEN=${gitlab_access_token}
 
 # per_page=100の意味がわかってない。リポジトリが１００までしか取得できないとか？
-for project_id in $(curl --header "PRIVATE-TOKEN: $TOKEN" "${GITLAB_URL}/projects?simple=true&per_page=100" | jq -r '.[].id'); do
+for project_id in $(curl --silent --header "PRIVATE-TOKEN: $TOKEN" "${GITLAB_URL}/projects?simple=true&per_page=100" | jq -r '.[].id'); do
   if [ "$project_id" == "125" ]; then # project_create
     echo "Skipping project with ID: $project_id"
     continue
@@ -18,6 +18,6 @@ for project_id in $(curl --header "PRIVATE-TOKEN: $TOKEN" "${GITLAB_URL}/project
   fi
 
   echo "Deleting project with ID: $project_id"
-  curl --request DELETE --header "PRIVATE-TOKEN: $TOKEN" "${GITLAB_URL}/projects/${project_id}"
+  curl --silent --request DELETE --header "PRIVATE-TOKEN: $TOKEN" "${GITLAB_URL}/projects/${project_id}"
 done
 sleep 1
