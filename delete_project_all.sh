77ebf5af7a59d3bc7486c1c8ad360449ca676c6c
tmoritoki0227 <tmoritoki0227@gmail.com>
2024-08-13 23:39:20 +0900

    Update delete_project_all.sh

--- delete_project_all.sh
+++ delete_project_all.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -ex
 
+GITLAB_URL="http://35.75.155.254/gitlab/api/v4" # ★ここは環境で変わるよ
+TOKEN="glpat-wTscsa5Y6VrMVHDA7DsP" # ★ここは環境で変わるよ
 # SSMの設定が必要
-GITLAB_URL="http://${gitlab_url}/gitlab/api/v4"
-TOKEN=${gitlab_access_token}
+# GITLAB_URL="http://${gitlab_url}/gitlab/api/v4"
+# TOKEN=${gitlab_access_token}
 
 # per_page=100の意味がわかってない。リポジトリが１００までしか取得できないとか？
 for project_id in $(curl --silent --header "PRIVATE-TOKEN: $TOKEN" "${GITLAB_URL}/projects?simple=true&per_page=100" | jq -r '.[].id'); do
